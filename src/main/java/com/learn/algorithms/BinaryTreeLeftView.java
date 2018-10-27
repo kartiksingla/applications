efd320d2f71ceea258fa23b3df5f3be744921df1
Kartik <singla.kartik1@gmail.com>
2018-10-27 19:21:54 +0530

    Binary Tree Left View

---
+++ src/main/java/com/learn/algorithms/BinaryTreeLeftView.java
@@ -0,0 +1,35 @@
+package com.learn.algorithms;
+
+/**
+ * Using recursive call 
+ * Time Complexity : O(n)
+ * @author kartik
+ *
+ */
+public class BinaryTreeLeftView {
+
+    Node rootNode;
+
+    int maxHeight = 0;
+
+    public BinaryTreeLeftView() {
+        rootNode = SampleTrees.getComplexUnEvenLengthTree();
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeLeftView tree = new BinaryTreeLeftView();
+        tree.printLeftView(tree.rootNode, 1);
+    }
+
+    private void printLeftView(Node node, int rowIndex) {
+        if (node == null)
+            return;
+        if (maxHeight < rowIndex) {
+            System.out.print(node.value + " ");
+            maxHeight = rowIndex;
+        }
+
+        printLeftView(node.left, rowIndex + 1);
+        printLeftView(node.right, rowIndex + 1);
+    }
+}
